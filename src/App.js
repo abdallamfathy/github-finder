3927b8c35aac3325e161e8d593ec446bb57820c6
abdallamfathy <abdallamfathy@gmail.com>
2022-09-22 18:07:48 +0200

    make pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import "./index.css"
-import { BrowserRouter as Router , Route } from 'react-router-dom'
+import { BrowserRouter as Router , Route, Routes } from 'react-router-dom'
 import Navbar from "./component/layout/Navbar";
 import Footer from "./component/layout/Footer";
-
+import Home from "./pages/Home";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <Router>
       <div className="flex flex-col justify-between h-screen">
         <Navbar/>
-        <main>
-          <Footer/> 
+        <main className="container mx-auto px-3 pb-12">
+          <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/about" element={<About/>}/>
+            <Route path="/notFound" element={<NotFound/>}/>
+            <Route path="/*" element={<NotFound/>}/>
+            </Routes> 
+        Content
         </main>
+          <Footer/> 
       </div>
     </Router>
   );
