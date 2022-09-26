88b13e71fd9f3d5e5c15d345d7225ff299ebb01c
abdallamfathy <abdallamfathy@gmail.com>
2022-09-26 12:40:08 +0200

    add view profile

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react';
+import GithubContext from '../context/github/GithubContext';
+import { useParams } from 'react-router-dom';
+
+
+
+
 
 const User = () => {
+
+    const {getUser , user} = useContext(GithubContext)
+    const params = useParams()
+    useEffect(() => {
+    getUser(params.login)
+    
+    }, [])
+    
   return (
-    <div>User</div>
+    <div>{user.login}</div>
   )
 }
 
